858e86d36b3c4c9ef6a9b32ad53bdb2d6cdf520d
Elena_Kusochkova <83718500+gnil000@users.noreply.github.com>
2022-11-18 02:56:03 +0300

    Add files via upload

---
+++ TamagochiConsole/TamagochiConsole/Cat.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+
+namespace TamagochiConsole
+{
+    public class Cat
+    {
+
+        public int eat { get; set; } = 100;
+        public string name { get; set; } = "Кисуня";
+        public int mood { get; set; } = 100;
+        public bool sex { get; set; } //false - cat   true - кошка
+        public bool isDead { get; set; } = false;
+
+
+        public Cat(){
+            this.eat = 100;
+            this.sex = true;
+        }
+
+        public Cat(string name, bool sex)
+        {
+            this.eat = 100;
+            this.name = name;
+            this.sex = sex;
+        }
+
+
+        public Cat(bool f) //ПОЛНЫЙ КАЛ БЛЯТЬ мне просто лень было делать нормально, хз, может когда-нибудь переделаю.
+        {
+            Console.Clear();
+            Console.SetCursorPosition(0, 3);
+            Assets.born();
+
+            Console.SetCursorPosition(75, 5);
+            Console.WriteLine("Вы впервые берёте свою кроху на ручки.");
+
+            Console.SetCursorPosition(70, 8);
+            Console.Write("Выберите пол котика."); //просим ввести пол котика
+            while (true)
+            {
+                Console.SetCursorPosition(70, 10);
+                Console.Write("1 - женский\t0 - мужской");
+                Console.SetCursorPosition(70, 12);
+                string sexx = Console.ReadLine();
+
+                Console.SetCursorPosition(70, 10); //затираем строку женский/мужской, остальное затирается потом
+                Console.Write("                             ");
+
+                //стираем строку введённую пользователем
+                Console.SetCursorPosition(70, 12);
+                Console.WriteLine(space(sexx));
+                if (sexx.Equals("0"))
+                {
+                    this.sex = false;
+                    break;
+                }
+                else if (sexx.Equals("1"))
+                {
+                    this.sex = true;
+                    break;
+                }
+                else
+                {
+                    Console.SetCursorPosition(70, 8);
+                    Console.WriteLine("Ты в глаза долбишься? Вводи ещё раз: ");
+                }
+            }
+            Console.SetCursorPosition(70, 8);
+            Console.WriteLine("Введите имя котика                        ");
+
+            Console.SetCursorPosition(70, 10);
+            this.name = Console.ReadLine(); //читает имя
+            Console.SetCursorPosition(70, 10);
+            Console.Write(space(name)); //затирает имя
+
+            Console.SetCursorPosition(70, 8);
+            Console.Write($"Прекрасно, вашу кисуню теперь зовут {this.name}");
+
+            //осматривается
+            Thread.Sleep(5000);
+            Console.Clear();
+            Console.SetCursorPosition(0, 3);
+            Assets.lookAround();
+            Console.SetCursorPosition(75, 5);
+            Console.WriteLine($"{name} осторожно осматривается");
+        }
+
+        public bool setEat(int dam)
+        {
+            eat += dam;
+            if (eat < 0)
+            {
+                Console.WriteLine("Кошка сдохла...(");
+                isDead = true;
+            }
+            if (eat > 110)
+            {
+                eat = 30;
+                return true;
+            }
+            return false;
+        }
+
+
+
+        public void catDead()
+        {
+            Console.Clear();
+            Console.SetCursorPosition(0, 3);
+            Assets.dead();
+            Console.SetCursorPosition(75, 13);
+            if (sex)
+                Console.WriteLine($"К сожалению {name} умерла от голода. Это конец...");
+            else
+                Console.WriteLine($"К сожалению {name} умер от голода. Это конец...");
+            Console.SetCursorPosition(0, 20);
+        }
+
+        private string space(string x)
+        {
+            string newStr = "";
+            foreach (char c in x)
+                newStr += " ";
+            return newStr;
+        }
+
+    }
+}
---
+++ TamagochiConsole/TamagochiConsole/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TamagochiConsole
+{
+
+    internal class Program
+    {
+        //static Cat cat;
+        static Random rand = new Random();
+
+        static void LineLeave(int count)
+        {
+            for (int i = 0; i < count; i++)
+                Console.WriteLine();
+        }
+
+
+        static void drawConsole(Cat cat, bool overeat = false, int n = -1)
+        {
+            //int left, top;
+            string mess;
+
+            Console.Clear();
+
+            Console.SetCursorPosition(55, 1);   //установка курсора
+            Console.WriteLine(cat.name);        //пишем имя кота
+
+            Console.SetCursorPosition(80, 10);  //установка курсора
+            Console.WriteLine("Еда: "+ cat.eat); //вывод количества еды
+
+            //+-+-+-+-+-+-+-+-+ПЕРЕЕЛ+-+-+-+-+-+-+-+-+
+            if (overeat)
+            {
+                Console.SetCursorPosition(0, 3);
+                Assets.spew();
+                Console.SetCursorPosition(75, 13);
+                if (cat.sex)
+                    Console.WriteLine($"{cat.name} сильно переела и выблевала большую часть еды.");
+                else
+                    Console.WriteLine($"{cat.name} сильно переел и выблевал большую часть еды.");
+                return;
+            }
+
+
+
+            Console.SetCursorPosition(0,3);
+
+            mess = Assets.arrDraw[rand.Next(0, 3)].Invoke();   //вывод картинки кота
+
+            LineLeave(3);                               //отступы
+
+            Console.SetCursorPosition(75, 13);   //установка курсора по игрику
+
+            Console.WriteLine(cat.name + " "+ mess);
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(150, 40);
+
+            Cat cat = new Cat(true); //создаём кота, выводим всю хуйню и осматриваемся
+
+            //Thread.Sleep(5000); //спать
+
+            Stopwatch st = new Stopwatch();
+            bool overeat = false;
+
+            st.Start();
+
+
+            //bool f = false;
+
+
+            while (true)
+            {
+
+
+                ////+-+-+-+-+-+-+-+-+-+-+-+-+ПАУЗА+-+-+-+-+-+-+-+-+-+-+-+-+-+ ХУЙ С НЕЙ ПОКА ЧТО
+                //Task.Factory.StartNew(() =>
+                //{
+                //    while (Console.ReadKey().Key != ConsoleKey.Escape) ;
+                //    if (f == false)
+                //    {
+                //        st.Stop();
+                //        f = true;
+                //        Console.WriteLine("Пауза нажата.");
+                //    }
+                //    else
+                //    {
+                //        f = false;
+                //        Console.WriteLine("Игра продолжена.");
+                //        st.Start();
+                //    }
+                //});
+
+
+                if (st.ElapsedMilliseconds >= 5000)
+                {
+                    cat.setEat(-5);
+                    if (cat.isDead)
+                    {
+                        st.Stop();
+                        cat.catDead();
+                        Console.Read();
+                        return;
+                    }
+                    drawConsole(cat);
+                    st.Restart();
+                }
+
+                Task.Factory.StartNew(() =>
+                {
+                    while (Console.ReadKey().Key != ConsoleKey.Q) ;
+                    overeat = cat.setEat(10);
+                    drawConsole(cat, overeat);
+                    st.Stop();
+                    Thread.Sleep(5000); //задержка чтобы прочитать
+                    st.Start();
+                });
+
+
+
+            }
+
+
+        }
+
+
+    }
+}
